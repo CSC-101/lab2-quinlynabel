90cc4a13362db3cac65f4e8e7e8af2cc83ebb77a
quinlynabel <qabel@calpoly.edu>
2026-04-16 14:21:09 -0700

    First Commit of Lab 2 Task 4

--- main.py
+++ main.py
@@ -36,3 +36,50 @@ answer1 = function2(3, 2, 1)  #What is the value of answer1? 1
 answer2 = function2(2, 3, 1)  #What is the value of answer2? 4
 answer3 = function2(2, 1, 3)  #What is the value of answer3? 6
 print (answer1, answer2, answer3)
+
+
+from typing import Optional  #gain access to the Optional [X] type hint
+
+def checked_access(L:list[int], idx:int) -> Optional[int]:
+   test = idx >= 0 and idx < len(L)  #What is the value of test on each call? Test is False on first call and test is True on second call
+   if test:       #What is this check preventing? an Index Error
+      return L[idx]
+   else:
+      return None
+
+first = checked_access ([1, 0, 1], 9)  #What is the value of first? None
+second = checked_access([1, 0, 1], 2)  #What is the value of second? 1
+print(first)
+print(second)
+
+
+def length_sum(L:list[str]) -> int:
+   if len(L) > 2:
+      result = len(L[0]) + len(L[1]) + len(L[2])  #For which call below is this statement evaluated? First
+   elif len(L) > 1:  #and what are the values being added? This + Is + The, 4+2+3
+      result = len(L[0]) +len(L[1])  #For which call below is this statement evaluated? Second
+   elif len(L)>0:  #and what are the values being added? Second + _ + call, 6+1+4
+      result = len(L[0])  #For which call below is this statement evaluated? Third
+   else:  #and what are the values being added? Another + Call, 7+4
+      result = 0
+   return result
+
+first = length_sum(["this", "is", "the", "first", "call"])
+second = length_sum(["second call"])
+third = length_sum(["another", "call"])
+print(first)
+print(second)
+print(third)
+
+
+def surprising(L:list[str], other:str) -> list[str]:
+   L.append(other.upper())
+   return L
+
+words = ["this", "is", "confusing", "code."]
+first = surprising(words, "Avoid")
+second = surprising(words, "such.")
+  #What is the value of words at this point? ['this', 'is', 'confusing', 'code', 'AVOID', 'SUCH.']]
+  #What are the values of first and second at this point? ['this', 'is', 'confusing', 'code.' 'AVOID', 'SUCH.'], ['this', 'is', 'confusing', 'code.', 'AVOID', 'SUCH.']
+  #What happened? Avoid and such were turned into uppercase and added to words. The original list was changed.
+print(first, second)
